1d5344a69fd4d436a62ce2ae308231d2182ad6ec
MarekVanis <marek.vanis@penta.cz>
2026-02-17 14:03:10 +0100

    Create main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import MarginCalculator from './margin-calculator'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <MarginCalculator />
+  </React.StrictMode>
+)
